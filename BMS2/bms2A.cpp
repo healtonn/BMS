0fecba8fa5a1f94f99f7e431c0c1da01c6c47e0e
Jakub Jochlik <jochlik@email.cz>
2018-12-15 23:59:26 +0100

    fixed buffer sizes

--- BMS2/bms2A.cpp
+++ BMS2/bms2A.cpp
@@ -8,9 +8,11 @@
 #include <string.h>
 #include <stdio.h>
 #include <cstdlib>
+#include <math.h>
 #include "reedsolomon/ecc.h"
 
 #define CHUNK_SIZE 255      //num of bytes of one msg chunk
+#define ORIGINAL_BYTES 146
 
 using namespace std;
 
@@ -29,18 +31,22 @@ int main(int argc, char** argv){
     outputFile.open(outputFileName.c_str());
     
     unsigned int inputFileSize = inputFile.tellg();      //input file in bytes
+    unsigned int numberOfInputChunks = (unsigned int)ceil((float)inputFileSize / ORIGINAL_BYTES);
     unsigned int lastChunkModif = getChunkModification(inputFileSize);
-    unsigned int finalFileSize = inputFileSize + lastChunkModif;   // output file size in bytes
-    cout << "input: " <<  inputFileSize << " modif: " << lastChunkModif << " final: " << finalFileSize << endl;
+    unsigned int finalFileSize = (numberOfInputChunks + 1) * CHUNK_SIZE;   // output file size in bytes
+    cout << "input: " <<  inputFileSize << " number of chunks input: " << numberOfInputChunks << " modif: " << lastChunkModif << " final: " << finalFileSize << endl;
 
     unsigned char* infoChunk = new unsigned char[CHUNK_SIZE];
+    unsigned char* inputFileContent = new unsigned char[numberOfInputChunks * CHUNK_SIZE];
+    unsigned char* outputFileContent = new unsigned char[finalFileSize];
 
-    *infoChunk = (char) lastChunkModif;
-    int test = infoChunk[0];
-    cout << "TEST: " << test << endl;
-    outputFile.write((char*)infoChunk, sizeof(unsigned char) * CHUNK_SIZE);
-    *infoChunk = (char) 42;
-    outputFile.write((char*)infoChunk, sizeof(unsigned char) * CHUNK_SIZE);
+    unsigned char c;
+    int i = 0;
+    while(inputFile >> c){
+        inputFileContent[i] = c;
+        i++;
+    }
+
     initialize_ecc();
 
     inputFile.close();
